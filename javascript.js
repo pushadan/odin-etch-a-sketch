b9a8d06ce96dc202b86319116085829a36deb995
pushadan <35050497+pushadan@users.noreply.github.com>
2024-01-18 21:32:34 -0800

    Implement change canvas size functionality

--- javascript.js
+++ javascript.js
@@ -1,27 +1,33 @@
 //TODO
 // fix dragging of screen color issue
-// add color picker
-// add canvas size changer
 
 
 //variables
 let isDragging;
 let paintColor = "black";
 let eraseColor;
-const colorPicker = document.querySelector(".colorPicker");
-const canvasContainer = document.querySelector(".canvasContainer");
+let firstLoad = true;
+const canvas   = document.querySelector(".canvas");
 const brushButton = document.querySelector(".brushButton");
 const eraseButton = document.querySelector(".eraseButton");
+const colorPicker = document.querySelector(".colorPicker");
+const canvasSize = document.querySelector(".canvasSize");
 
 
+//firstLoad --------------
+createCanvas(16,16);
+firstLoad = false;
+
+
+//brush button
 brushButton.addEventListener("click", ()=>{
     paintColor = colorPicker.value;
 });
-
+//erase button
 eraseButton.addEventListener("click", ()=>{
     paintColor = "white";
 });
-
+//color picker button
 colorPicker.addEventListener("input", ()=>{
     paintColor = colorPicker.value;
 });
@@ -29,17 +35,40 @@ colorPicker.addEventListener("input", ()=>{
 
 
 
-//create canvas bc manually creating it in my html is stupid
-for (let i=1; i<=256; i++){
-    let pixel = document.createElement("div");
-    pixel.classList.add("pixel")
-    canvasContainer.appendChild(pixel);
-};
+//functions --------------------------
 
-//capture all .pixel divs AFTER they are created.
-let pixel = [] = document.querySelectorAll(".pixel");
-//add event listeners to all .pixel divs
-pixel.forEach((element) => {
+//createCanvas function. Recieves canvase # of pixel width and height
+function createCanvas(width, height){
+    //clear canvas if it's not the first load
+    if (!firstLoad){
+        //capture all .pixel divs AFTER they are created.
+        let pixel = [] = document.querySelectorAll(".pixel");
+        pixel.forEach((element)=>{
+            canvas.removeChild(element);
+        });
+    }
+    
+    //find total # of pixels
+    canvasTotalPixels = width * height;
+    //set width of canvas
+    canvas.style.width = `${width*20}px`;
+
+    //debug pixel amount
+    let pixelCount = 0;
+    //create pixels, add to Canvas
+    for (let i=0; i<canvasTotalPixels; i++){
+        let pixel = document.createElement("div");
+        pixel.classList.add("pixel")
+        canvas.appendChild(pixel);
+        pixelCount++
+    };
+    //debug pixels amount
+    console.log(pixelCount);
+    //capture all .pixel divs in memory AFTER they are created.
+    let pixel = [] = document.querySelectorAll(".pixel");
+
+    //add "mousedown", "mousemove", "mouseup" event listeners to all .pixel divs
+    pixel.forEach((element) => {
     //capture clicking + holding down
     element.addEventListener("mousedown", (e)=>{
         isDragging = true;
@@ -56,4 +85,5 @@ pixel.forEach((element) => {
         isDragging = false;
     });
 });
+};
 
